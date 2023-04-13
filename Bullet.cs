c04d2dddc8fa28919d399c51560f19fc79c2218f
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-13 21:45:12 +0900

    주석 업데이트
    bullet 완료

--- Bullet.cs
+++ Bullet.cs
@@ -26,24 +26,24 @@ public class Bullet : MonoBehaviourPun
     }
     public void Fire(float _firespeed,int layer)
     {
-        speed += _firespeed;
-        gameObject.layer = layer;
-        therigid.velocity = transform.forward * speed;
+        speed += _firespeed; 
+        gameObject.layer = layer; //플레이어와 총알이 겹치지 않게 하기위한 레이어
+        therigid.velocity = transform.forward * speed; //총알을 스피드만큼 전방으로 이동시킴
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag.Equals("Player"))
+        if (other.tag.Equals("Player")) //player 와 닿았을 경우
         {
             //
-            if (other.gameObject.GetComponent<PhotonView>().Owner.ActorNumber.Equals((int)other.gameObject.GetComponent<PlayerController>().mn))
+            if (other.gameObject.GetComponent<PhotonView>().Owner.ActorNumber.Equals((int)other.gameObject.GetComponent<PlayerController>().mn)) //상대 플레이어라면
             {
-                theplayer.guneffectaudio.clip = theplayer.missileclip;
-                theplayer.guneffectaudio.Play();
+                theplayer.guneffectaudio.clip = theplayer.missileclip; 
+                theplayer.guneffectaudio.Play(); //사운드 재생
 
                 Explode();
 
-                other.gameObject.GetComponent<HP>().Damage(4);
-                Destroy(this.gameObject);
+                other.gameObject.GetComponent<HP>().Damage(4); //hp 감소
+                Destroy(this.gameObject); //총알 파괴
             }
         }
 
@@ -55,6 +55,6 @@ public class Bullet : MonoBehaviourPun
     }
     void Explode()
     {
-        PhotonNetwork.Instantiate("effect", transform.position, Quaternion.identity, 0);
+        PhotonNetwork.Instantiate("effect", transform.position, Quaternion.identity, 0); //서버에서 이펙트 생성하여 다른 플레이어에게도 이펙트가 보이게함
     }
 }
