fb5f0a7b54fc1fa1bec2b1125930f25e692a0316
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-17 14:56:14 +0900

    주석 업데이트

--- MultyManager.cs
+++ MultyManager.cs
@@ -9,24 +9,24 @@ public class MultyManager : MonoBehaviourPunCallbacks
     public GameObject theplayer1;
     public GameObject theplayer2;
     // Start is called before the first frame update
-    private void Awake()
+    private void Awake() //start 보다 먼저 실행
     {
-        float pos1 = Random.Range(7000, 10000);
+        float pos1 = Random.Range(7000, 10000); //플레이어 위치를 램덤으로 지정
         float pos2 = Random.Range(6000, 9000);
 
-        if (!PhotonNetwork.IsMasterClient)
+        if (!PhotonNetwork.IsMasterClient) //만약 첫번째로 들어와 서버의 마스터라면
         {
-            theplayer1 = PhotonNetwork.Instantiate("Player1", new Vector3(pos1, 1000, pos1), Quaternion.identity, 0) as GameObject;
+            theplayer1 = PhotonNetwork.Instantiate("Player1", new Vector3(pos1, 1000, pos1), Quaternion.identity, 0) as GameObject; //플레이어 1, 2 두객체를 우선 생성
             theplayer2 = PhotonNetwork.Instantiate("Player2", new Vector3(pos2, 1000, pos2), Quaternion.identity, 0) as GameObject;
 
-            pv.RPC("take_owner", RpcTarget.Others, theplayer1.GetComponent<PhotonView>().ViewID, theplayer2.GetComponent<PhotonView>().ViewID);
+            pv.RPC("take_owner", RpcTarget.Others, theplayer1.GetComponent<PhotonView>().ViewID, theplayer2.GetComponent<PhotonView>().ViewID); //그중하나으의 객체를 다른 플레이어에게 소유권을 넘김
         }
     }
     private void Start()
     {
         Screen.SetResolution(1920, 1080, true);
     }
-
+    //다른 플레이어에게 소유권을 넘기는 함수
     [PunRPC]
     void take_owner(int pvid1, int pvid2)
     { //MultyManager.cs take_owner() 수정
