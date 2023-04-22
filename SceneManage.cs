0700dd653f15b8702c992065b78832852918887b
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-22 09:36:54 +0900

    주석 업데이트

--- SceneManage.cs
+++ SceneManage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-public class SceneManage : MonoBehaviourPunCallbacks
+public class SceneManage : MonoBehaviourPunCallbacks //게임 시작하기전에 컷신
 {
     private float FadeTime = 2f;
     public Image fadeimg;
@@ -15,7 +15,7 @@ public class SceneManage : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     private void moving()
     {
-        therigid.AddForce(Vector3.left * 50);
+        therigid.AddForce(Vector3.left * 50); //왼쪽으로 점차 가속시킴
     }
     void Start()
     {
@@ -27,12 +27,12 @@ public class SceneManage : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        moving();
+        moving(); //매프레임마다 실행
     }
     IEnumerator loadsceneco()
     {
-        yield return new WaitForSeconds(5f);
-        OutStartFadeAnim();
+        yield return new WaitForSeconds(5f); //5초가 지난후에
+        OutStartFadeAnim(); //함수 실행
         
 
     }
@@ -42,7 +42,7 @@ public class SceneManage : MonoBehaviourPunCallbacks
             return;
         end = 1f;
         start = 0f;
-        StartCoroutine("fadeoutplay");
+        StartCoroutine("fadeoutplay"); //페이드 아웃실행
     }
     public void InStartFadeAnim()
     {
@@ -50,7 +50,7 @@ public class SceneManage : MonoBehaviourPunCallbacks
             return;
         start = 1f;
         end = 0f;
-        StartCoroutine("fadeInanim");
+        StartCoroutine("fadeInanim"); //페이드 인실행
     }
     IEnumerator fadeoutplay()
     {
@@ -63,12 +63,12 @@ public class SceneManage : MonoBehaviourPunCallbacks
         while (fadecolor.a <1f)
         {
             time += Time.deltaTime / FadeTime;
-            fadecolor.a = Mathf.Lerp(start, end, time);
+            fadecolor.a = Mathf.Lerp(start, end, time); //색깔을 점점 어둡게함
             fadeimg.color = fadecolor;
             yield return null;
         }
         isplaying = false;
-        PhotonNetwork.LoadLevel("Gamescene");
+        PhotonNetwork.LoadLevel("Gamescene"); //게임시작함
 
     }
     IEnumerator fadeInanim()
@@ -82,7 +82,7 @@ public class SceneManage : MonoBehaviourPunCallbacks
         while(fadecolor.a>0f)
         {
             time += Time.deltaTime / FadeTime;
-            fadecolor.a = Mathf.Lerp(start, end, time);
+            fadecolor.a = Mathf.Lerp(start, end, time); //색깔을 점점 밝게함
             fadeimg.color = fadecolor;
             yield return null;
         }
