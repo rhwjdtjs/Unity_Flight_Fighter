46f17c7f5505be079faa8707f779ade240d9ccb2
rhwjdtjs <42109688+rhwjdtjs@users.noreply.github.com>
2023-04-24 14:25:54 +0900

    주석 업데이트

--- TargetInfoUI.cs
+++ TargetInfoUI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
-public class TargetInfoUI : MonoBehaviourPun
+public class TargetInfoUI : MonoBehaviourPun //타겟의 위치를 네모 아이콘으로 표시해주는 스크립트
 {
     [SerializeField] public Text TargetPosText;
     [SerializeField] public Text cantseeTargetposTEXT;
@@ -42,7 +42,7 @@ public class TargetInfoUI : MonoBehaviourPun
     // Update is called once per frame
     private void LateUpdate()
     {
-        targettransform = themul.theplayer1.transform;
+        targettransform = themul.theplayer1.transform; //타겟의 위치를 매번 계산
         if (photonView.IsMine)
             TargetUIFollow();
     }
@@ -50,23 +50,23 @@ public class TargetInfoUI : MonoBehaviourPun
    
     private void TargetUIFollow()
     {
-        targetpos = (targettransform.position - playertransform.position).magnitude;
-        TargetPosText.text = targetpos.ToString("F1") + "M";
-        cantseeTargetposTEXT.text = targetpos.ToString("F1") + "M";
+        targetpos = (targettransform.position - playertransform.position).magnitude; //타겟의 위치와 자신의 위치 사이의 거리를 구함
+        TargetPosText.text = targetpos.ToString("F1") + "M"; //그값을 표시
+        cantseeTargetposTEXT.text = targetpos.ToString("F1") + "M"; //전방에 없는 적의 거리를 계산하고 표시
 
-         screenPos = camera.WorldToScreenPoint(targettransform.position);
+         screenPos = camera.WorldToScreenPoint(targettransform.position); //월드 좌표를 평면으로 구함
      
-         targetimage.transform.position = new Vector3(screenPos.x, screenPos.y, 0);
+         targetimage.transform.position = new Vector3(screenPos.x, screenPos.y, 0); //타겟 위치를 보여주는 이미지의 위치 표시
 
-        rockonimage.transform.position = new Vector3(screenPos.x, screenPos.y, 0);
-        if (screenPos.z<0)
+        rockonimage.transform.position = new Vector3(screenPos.x, screenPos.y, 0); //라공ㄴ시에도 마찬가지로 표시
+        if (screenPos.z<0) //적이 전방에 없을때 맞는 아이콘 표시
         {
             targetimage.gameObject.SetActive(false);
             targetTGT.gameObject.SetActive(false);
             cantseetarget.SetActive(true);
             rockonimage.gameObject.SetActive(false);
         }
-        else
+        else //적이 전방에 있을때
         {
             if (!therockon.CheckSight())
             {
